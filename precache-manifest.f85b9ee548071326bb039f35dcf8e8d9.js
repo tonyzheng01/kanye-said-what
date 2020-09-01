64d8c0936541ab51e376fb19ce3b47d8ae754c7e
Tony Zheng <tony.zheng@uconn.edu>
2020-08-31 21:18:55 -0400

    Updates

---
+++ precache-manifest.f85b9ee548071326bb039f35dcf8e8d9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00eae9f8c3ef106baddfd845369b6d82",
+    "url": "/kanye-said-what/index.html"
+  },
+  {
+    "revision": "a809cb4ff378769dc106",
+    "url": "/kanye-said-what/static/css/main.21197044.chunk.css"
+  },
+  {
+    "revision": "944452ab8cf26a4896e6",
+    "url": "/kanye-said-what/static/js/2.8889cb55.chunk.js"
+  },
+  {
+    "revision": "a5a4149a938b394962bf238e6c1aaf1a",
+    "url": "/kanye-said-what/static/js/2.8889cb55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a809cb4ff378769dc106",
+    "url": "/kanye-said-what/static/js/main.7945de98.chunk.js"
+  },
+  {
+    "revision": "a887afe772f32ebfadbc",
+    "url": "/kanye-said-what/static/js/runtime-main.0255550e.js"
+  },
+  {
+    "revision": "0a6d06fd7e89df08ddab4eb6cb0afd27",
+    "url": "/kanye-said-what/static/media/face.0a6d06fd.png"
+  },
+  {
+    "revision": "f9007ffe5f8bc332c4111d56133819fb",
+    "url": "/kanye-said-what/static/media/lyrics.f9007ffe.csv"
+  },
+  {
+    "revision": "badbb7ef8f80ed87e510c4a3d7d20106",
+    "url": "/kanye-said-what/static/media/maybe.badbb7ef.jpg"
+  },
+  {
+    "revision": "2c454c9cd46f63811313daac399370f1",
+    "url": "/kanye-said-what/static/media/nope.2c454c9c.jpg"
+  },
+  {
+    "revision": "637686865e10ba3b95453f4cd6fe7d16",
+    "url": "/kanye-said-what/static/media/yup.63768686.jpg"
+  }
+]);
