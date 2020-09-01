00561620e0f2fd92a18978080ce2c25cb59aac3f
Tony Zheng <tony.zheng@uconn.edu>
2020-08-31 20:42:46 -0400

    Updates

---
+++ precache-manifest.03d5d6642b7f382672574f0fec027346.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac3d614c98d79034fcf767034896de4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7905e9c8f2ecd62d3813",
+    "url": "/static/css/main.21197044.chunk.css"
+  },
+  {
+    "revision": "42954e580d0a2892b056",
+    "url": "/static/js/2.2029104a.chunk.js"
+  },
+  {
+    "revision": "a5a4149a938b394962bf238e6c1aaf1a",
+    "url": "/static/js/2.2029104a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7905e9c8f2ecd62d3813",
+    "url": "/static/js/main.bd539fd2.chunk.js"
+  },
+  {
+    "revision": "cc15668519d12fa70501",
+    "url": "/static/js/runtime-main.b78cfb73.js"
+  },
+  {
+    "revision": "0a6d06fd7e89df08ddab4eb6cb0afd27",
+    "url": "/static/media/face.0a6d06fd.png"
+  },
+  {
+    "revision": "5165b61351d5d97bec7a062079796c9e",
+    "url": "/static/media/lyrics_test.5165b613.csv"
+  },
+  {
+    "revision": "badbb7ef8f80ed87e510c4a3d7d20106",
+    "url": "/static/media/maybe.badbb7ef.jpg"
+  },
+  {
+    "revision": "2c454c9cd46f63811313daac399370f1",
+    "url": "/static/media/nope.2c454c9c.jpg"
+  },
+  {
+    "revision": "637686865e10ba3b95453f4cd6fe7d16",
+    "url": "/static/media/yup.63768686.jpg"
+  }
+]);
